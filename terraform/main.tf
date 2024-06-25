0f43b403dfcebd36a1042068fbafc97629b0c39f
Joseph DuBay <dubie1977@gmail.com>
2024-06-25 12:34:36 -0700

    1

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,20 +5,20 @@ terraform {
     }
   }
 
-	backend "s3" {
-		region = "us-west-2"
-		key    = "terraform.tfstate"
-	}
+  backend "s3" {
+    region = "us-west-2"
+    key    = "terraform.tfstate"
+  }
 }
 
 provider "aws" {
-	egion = "us-west-2"
+   region = "us-west-2"
 }
 
 resource "aws_instance" "test_instance" {
-	ami           = "ami-830c94e3"
-	instance_type = "t2.nano"
-	tags = {
-		Name = "test_instance"
-	}
+  ami           = "ami-830c94e3"
+  instance_type = "t2.nano"
+  tags = {
+    Name = "test_instance"
+  }
 }
