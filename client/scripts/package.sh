4c124e55a46327dd24e66121f824c3bdf9a5fbef
Joseph DuBay <dubie1977@gmail.com>
2024-06-28 09:48:25 -0700

    1

--- client/scripts/package.sh
+++ client/scripts/package.sh
@@ -9,10 +9,12 @@ echo "node type"
 npm i -D @types/node
 echo "buiilding"
 npm run build
+pwd
 ls
 mkdir dist
 cp -r ./*.js dist/
 cp -r ./*.css dist/
 cp -r ./*.svg dist/
+ls
 # cd dist
 # zip -r ${WORKDIR}/../add_task_lambda_function.zip .
