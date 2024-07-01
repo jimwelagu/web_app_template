8af66df1a7077286e34353a59faf1bf9e3e4aeeb
Joseph DuBay <dubie1977@gmail.com>
2024-07-01 16:02:47 -0700

    1

--- terraform/modules/react_ui/main.tf
+++ terraform/modules/react_ui/main.tf
@@ -105,11 +105,11 @@ module "cloudfront" {
   # aliases = ["reactdemo.takemetoprod.com"]
 }
 
-resource "aws_s3_object" "uploaded_object" {
-  for_each = fileset(var.client_path, "*")
-  bucket = module.s3_bucket.s3_bucket_id
-  key    = each.value
-  source = "${var.client_path}/${each.value}"
-  etag   = filemd5("${var.client_path}/${each.value}")
-  content_type = "text/html"
-}
+# resource "aws_s3_object" "uploaded_object" {
+#   for_each = fileset(var.client_path, "*")
+#   bucket = module.s3_bucket.s3_bucket_id
+#   key    = each.value
+#   source = "${var.client_path}/${each.value}"
+#   etag   = filemd5("${var.client_path}/${each.value}")
+#   content_type = "text/html"
+# }
